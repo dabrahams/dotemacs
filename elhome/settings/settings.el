a0c807a00d301c273c572ae21325225f087f5638
Dave Abrahams <dabrahams@apple.com>
2013-12-02 11:20:39 -0800

    Keep elhome configuration in source control; stop writing tilde-turds everywhere.

---
+++ elhome/settings/settings.el
@@ -0,0 +1,9 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(backup-by-copying t)
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
+ '(gud-gdb-command-name "gdb --annotate=1")
+ '(large-file-warning-threshold nil))
