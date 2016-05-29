c6922b31de9a6efa64a11d9c689de47eb7c2c350
Dave Abrahams <dabrahams@apple.com>
2016-05-28 18:10:51 -0700

    Remove grep-settings; nothing intelligible there

--- elhome/settings/grep-settings.el
+++
@@ -1,6 +0,0 @@
-(require 'grep-ed nil 'noerror)
-
-(cl-flet ((set-face-foreground (&rest args))
-       (set-face-background (&rest args)))
-  (let (grep-hit-face)
-    (require 'grep+ nil 'noerror)))
