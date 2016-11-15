c55d8ea149a0ab11e507fac7dd1c575cd0ecda89
Dave Abrahams <dabrahams@apple.com>
2016-11-14 20:31:37 -0800

    I don't use marked for Markdown anymore

--- elhome/settings/markdown-settings.el
+++ elhome/settings/markdown-settings.el
@@ -1,11 +1,3 @@
-(defun markdown-preview-file ()
-  "Run Marked on the current file and revert the buffer"
-  (interactive)
-  (shell-command 
-   (format "open -a /Applications/Marked.app %s" 
-           (shell-quote-argument (buffer-file-name)))))
-
-(define-key mode-specific-map [?m] 'markdown-preview-file)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
