9d171aa92c115d7a74c669145da2178d72523e68
Dave Abrahams <dabrahams@google.com>
2020-09-09 14:33:25 -0700

    No more flymake by default.

--- elhome/settings/prog-mode-settings.el
+++ elhome/settings/prog-mode-settings.el
@@ -3,10 +3,10 @@
   (ignore-errors (fci-mode t) (whitespace-mode -1))
     ;; Turn on flymake in this buffer, but only *after* local
     ;; variables have been set up.
-    (run-with-idle-timer 
-     0.1 (not :repeat) 
-     (lambda (b) (ignore-errors (with-current-buffer b (flymake-mode 1))))
-     (current-buffer))
+    ;; (run-with-idle-timer 
+    ;;  0.1 (not :repeat) 
+    ;;  (lambda (b) (ignore-errors (with-current-buffer b (flymake-mode 1))))
+    ;;  (current-buffer))
   ;; These are wrong as they are enabled globally.  Just put them in 99-sugar if you want that.
   ;; (electric-pair-mode t)
   ;; (electric-layout-mode t)
